eff80bca7b32b9646d9e76adc74eb4e2eba1afb2
artem.khrolenko <artem.khrolenko@gmail.com>
2019-05-17 11:39:27 +0300

    Commit

--- TechDotNetLib/Lab/Substances/Acetonitrile.cs
+++ TechDotNetLib/Lab/Substances/Acetonitrile.cs
@@ -105,7 +105,7 @@ namespace TechDotNetLib.Lab.Substances
         //Метод для определения концентрации вещества в N-компонентной смеси
         public override double GetContent(float temperature, float pressure)
         {
-            return temperature - WspLib.Tsat(pressure) * 100 / (1670.409 / (5.37229 - Math.Log10((pressure) * 0.98717)) - 232.959 - WspLib.Tsat(pressure));
+            return (temperature - WspLib.Tsat(pressure)) * 100 / (1670.409 / (5.37229 - Math.Log10((pressure) * 0.98717)) - 232.959 - WspLib.Tsat(pressure));
         }
 
         //Расчет давления насыщенного пара при заданной температуре, бар, абс.
